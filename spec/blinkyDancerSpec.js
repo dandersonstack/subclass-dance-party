fddbf4963fa665883f9587a625faa344e4bd4fcd
Hack Reactor Student <student@hackreactor.com>
2017-05-22 16:54:32 -0700

    changed the test so it starts blinking from time 0 rather than time 100

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -20,10 +20,9 @@ describe('blinkyDancer', function() {
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
-      debugger;
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      // clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
       clock.tick(timeBetweenSteps);
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
